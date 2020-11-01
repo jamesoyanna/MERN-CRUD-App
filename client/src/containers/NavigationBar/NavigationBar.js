aef64213af0510cf28a8d410baf154f1a224bef0
james <engrjayt200@gmail.com>
2020-11-01 23:32:28 +0100

    Implement redux action

---
+++ client/src/containers/NavigationBar/NavigationBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <div>
+               <h2>This is Navigation bar</h2> 
+            </div>
+        );
+    }
+}
+
+export default NavigationBar;
