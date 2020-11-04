5a7a6ed65e05c32a8d5590c2d85799c8353eb168
james <engrjayt200@gmail.com>
2020-11-04 13:13:28 +0100

    Update Article component

--- client/src/index.js
+++ client/src/index.js
@@ -6,24 +6,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, combineReducers } from "redux";
-//import articlesReducer from "./store/reducers/articlesReducer";
-//import usersReducer from "./store/reducers/usersReducer";
+import articlesReducer from "./store/reducers/articlesReducer";
+import usersReducer from "./store/reducers/usersReducer";
 
 // import registerServiceWorker from './registerServiceWorker';
 
-// const rootReducer = combineReducers({
-//   articles: articlesReducer,
-//   users: usersReducer,
-// });
+const rootReducer = combineReducers({
+  articles: articlesReducer,
+  users: usersReducer,
+});
 
-//const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const app = (
-//   <Provider store={store}>
+  <Provider store={store}>
     <Router>
      <App />
    </Router>
-//   </Provider>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
---
+++ config.js
@@ -0,0 +1,9 @@
+const JWTSECRET = process.env.JWTSECRET;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+module.export = {
+  jwtSecret: JWTSECRET,
+
+  mongodburi: ""
+};
